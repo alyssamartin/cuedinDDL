d4ae3c107b043d60fbe52f6c4ce10fee6ad45176
LabPatron <labpatron@ad.jmu.edu>
2019-03-29 14:14:46 -0400

    updated trigger

--- DatabaseDependencies/StudentHoursTrigger.sql
+++ DatabaseDependencies/StudentHoursTrigger.sql
@@ -15,9 +15,15 @@ Declare @CounselorApproval varchar,
 select @CounselorApproval=(select [CounselorApproval] from inserted)
 select @HoursWorked=(select [HoursRequested] from inserted)
 select @StudentEntityID=(select StudentEntityID from inserted)
-select @StudentEmploymentFlag=(Select [StudentEmploymentFlag] from student)
+select @StudentEmploymentFlag=(Select [StudentEmploymentFlag] from student where StudentEntityID = @StudentEntityID)
 
 if @CounselorApproval = 'Y'
 begin
-update Student set [HoursOfWorkPlaceExp] = [HoursOfWorkPlaceExp] + 
+update Student set [HoursOfWorkPlaceExp] = [HoursOfWorkPlaceExp] + @HoursWorked where studentEntityID =  @StudentEntityID
+end
 
+if @CounselorApproval = 'Y' and @StudentEmploymentFlag = 'N'
+begin 
+update Student set [StudentEmploymentFlag] = 'Y' where StudentEntityID = @StudentEntityID
+end
+end
